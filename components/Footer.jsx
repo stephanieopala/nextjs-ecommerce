c56611335862bba5fdb5ccd990a065a5df83a004
Stephanie Opala <rispahopala@gmail.com>
2023-12-07 18:37:54 +0300

    feat(footer): add footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,45 @@ import styles from '../styles/Footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      Kwetu &copy; 2022
+      <hr className={styles.footerBreak}/>
+      <div className={styles.top}>
+        <div>
+          <p className={styles.logo}>Kwetu Furniture</p>
+          <p className="text-sm">Designed and manufactured in Kenya</p>
+        </div>
+        <div className={styles.container}>
+          <p>About</p>
+          <ul>
+            <li>
+              Shop
+            </li>
+            <li>
+              Cart
+            </li>
+          </ul>
+        </div>
+        <div className={styles.container}>
+          <p>Get in touch</p>
+          <ul>
+            <li>
+              LinkedIn
+            </li>
+            <li>
+              Instagram
+            </li>
+            <li>
+              Twitter
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.bottom}>
+        <p>@2023.All rights reserved</p>
+        <div>
+          <p>Privacy & Policy</p>
+          <p>Terms & Conditions</p>
+        </div>
+      </div>
     </footer>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,39 @@ const HomePage = () => {
           />
         </div>
       </div>
+        <section className={styles.products}>
+          <h2>Our Products</h2>
+          <div className={styles.categories}>
+            <div className={styles.card}>
+              <Image
+                alt="furniture"
+                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/v1666542643/ecommerce/grey-armchair_l1jiue.jpg`}
+                width={300}
+                height={300}
+              />
+              <p className={styles.cardTitle}>Furniture</p>
+              <p>Explore a variety of furniture. From house furniture to office furnture.</p>
+            </div>
+            <div className={styles.card}>
+              <Image alt="furniture"
+                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/v1666542667/ecommerce/white-vase_uffcep.jpg`}
+                width={300}
+                height={300}
+              />
+              <p className={styles.cardTitle}>Accessories</p>
+              <p>Explore various accessories for styling your home or office</p>
+            </div>
+            <div className={styles.card}>
+              <Image
+                alt="furniture"
+                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/v1666542550/ecommerce/green-couch_qzwzuy.jpg`}
+                width={300}
+                height={300} />
+              <p className={styles.cardTitle}>Custom made</p>
+              <p>Get custom-made pieces for your home or office.</p>
+            </div>
+          </div>
+        </section>
     </>
   )
 }
